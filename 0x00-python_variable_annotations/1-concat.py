757f1a086edc14a63e492f65e15e7984d05275bf
Joakim-Animate90 <joakimbwire23@gmail.com>
2023-01-26 20:25:04 +0300

    Update task 1 and build task 2

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-'''Task 2's module.
+'''Task 1's module.
 '''
 
 
-def floor(a: float) -> int:
-    '''Computes the floor of a floating-point number.
+def concat(str1: str, str2: str) -> str:
+    '''Concatenates two strings.
     '''
-    return int(a)
+    return str1 + str2
