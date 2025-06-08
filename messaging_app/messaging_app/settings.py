4b500bc5331dff1d76a199bc81d7402f476f3b6b
Joakim Animate <joakimbwire23@gmail.com>
2025-06-08 14:28:57 +0300

    Refactor messaging app structure by removing old files and adding updated ASGI, WSGI, settings, and URL configurations

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'chats',  # Your app for handling conversations and messages
+    
 ]
 
 MIDDLEWARE = [
